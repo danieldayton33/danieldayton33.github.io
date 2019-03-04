a800b73cdbc0569e936fa9df1de111e473af2781
danieldayton33 <danieldayton33@gmailcom>
2019-03-04 15:51:45 -0500

    added carousel

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
+$("#name-header").on("click", ()=>{
+    $("#about").hide();
+    $("#portfolio").hide();
+    $("#start-slider").show();
+});
 $("#about-button").on("click", ()=>{
+    $("#start-slider").hide();
     $("#portfolio").hide();
     $("#about").show();
 });
 $("#portfolio-button").on("click", ()=>{
+    $("#start-slider").hide();
     $("#about").hide();
     $("#portfolio").show();
 });
 $("#contact-button").on("click", ()=>{
+    $("#start-slider").hide();
     $("#about").hide();
     $("#portfolio").hide();
     $("#contact-modal").modal('toggle');
 });
-// $("#mail-submit").on("click", ()=>{
-//     $(".modal-body").empty();
-//     const newP = $("<p>").text("Your email has been sent");
-//     $(".modal-body").append(newP);
-// });
+$("#carousel").on("click", ()=>{
+    $("#start-slider").hide();
+    $("#about").hide();
+    $("#portfolio").show();
+});
+$("#mail-submit").on("click", ()=>{
+    $(".modal-body").empty();
+    const newP = $("<p>").text("Your email has been sent");
+    $(".modal-body").append(newP);
+});
 });
