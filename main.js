4fb02b00ad8b5f514e5f53abe88b92cbca913f79
danieldayton33 <danieldayton33@gmailcom>
2019-03-04 20:32:24 -0500

    final UI

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
-$("#name-header").on("click", ()=>{
+const buttonArray = ["CSS", "Javascript", "HTML", "nodeJS", "APIs"]
+
+const createButtons = ()=> {
+    buttonArray.forEach(category =>{
+        const newButton = $('<button>',
+            {
+            type: 'button',
+            class: 'btn btn-light',
+            id: 'filter-button',
+            text: category,
+            on: {click : ()=>{
+                clearCards(category);
+                }
+            }
+            });
+    $('#filter-buttons').append(newButton);  
+    });
+}
+createButtons();
+
+const clearCards = (category)=> {
+    console.log(category);
+    if($(".card").hasClass(category) === false){
+        console.log("HAS CLASS");
+        console.log($(this));
+        $(this).hide();
+    }
+    if($(".card").hasClass(category)){
+        console.log("DOESNOT HAVE CLASS");
+        console.log($(this));
+        $(this).hide();
+}
+}
+//on-click functions to hide different sections
+    $("#name-header").on("click", ()=>{
     $("#about").hide();
     $("#portfolio").hide();
     $("#start-slider").show();
@@ -15,12 +49,12 @@ $("#portfolio-button").on("click", ()=>{
     $("#portfolio").show();
 });
 $("#contact-button").on("click", ()=>{
-    $("#start-slider").hide();
+    $("#start-slider").show();
     $("#about").hide();
     $("#portfolio").hide();
     $("#contact-modal").modal('toggle');
 });
-$(".carousel-pic").on("click", ()=>{
+$(".carousel-inner").on("click", ()=>{
     $("#start-slider").hide();
     $("#about").hide();
     $("#portfolio").show();
@@ -29,5 +63,6 @@ $("#mail-submit").on("click", ()=>{
     $(".modal-body").empty();
     const newP = $("<p>").text("Your email has been sent");
     $(".modal-body").append(newP);
+
 });
 });
